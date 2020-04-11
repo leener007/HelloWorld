a7b9d48d9df0cae96c7320fc628317dee56f8145
leener007 <834675896@qq.com>
2020-04-11 18:01:10 +0800

    123123

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
+// const maxChunksToSorted = function (arr) {
+//     let len = arr.length -1
+//     let myArr = new Array(len)
+//     for (let k = len; k >= 0; k--){
+//         myArr[k] = 0
+//     }
+//     let i = 0
+//     let result = 0
+//     while(i < len) {
+//         let max = 0
+//         if (arr[i] === i) {
+//             result++
+//             i++
+//         } else if (myArr[i] === 1) {
+//             result++
+//             i = max + 1
+//         } else {
+//             max = Math.max(max, i)
+//             myArr[i] = 1
+//             i = arr[i]
+//         }
+//     }
+//     return result
+// }
+// let a = [1,0,2,3,4]
+// console.log(maxChunksToSorted(a))
 console.log(123)
